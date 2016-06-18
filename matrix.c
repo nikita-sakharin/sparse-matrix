043fe096f736994fb20205e5067823908f474bc4
nikita1997 <nikitasa1997@gmail.com>
2016-06-18 22:28:16 +0300

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <complex.h>
+#include <math.h>
 #include "matrix.h"
+
+typedef struct pair Pair;
+
+struct pair
+{
+	size_t column;
+	long double complex value;
+};
 
 struct matrix
 {
-	size_t row_count, *row, *col;
-	long double complex *data;
+	size_t row_count, *row;
+	Pair *data;
 };
+
+int matrix_elem_insert(Matrix * restrict matrix, size_t row_idx, size_t col_idx, long double complex value);
+
+int matrix_elem_delete(Matrix * restrict matrix, size_t row_idx, size_t col_idx);
 
 Matrix *matrix_create(void)
 {
@@ -26,19 +39,14 @@ int matrix_destroy(Matrix ** restrict matrix)
 		return EOF;
 	}
 
-	if ((*matrix)->row != NULL)
-	{
-		free((*matrix)->row);
-	}
-
-	if ((*matrix)->col != NULL)
-	{
-		free((*matrix)->col);
-	}
-
 	if ((*matrix)->data != NULL)
 	{
 		free((*matrix)->data);
+	}
+
+	if ((*matrix)->row != NULL)
+	{
+		free((*matrix)->row);
 	}
 
 	free(*matrix);
@@ -46,187 +54,6 @@ int matrix_destroy(Matrix ** restrict matrix)
 	*matrix = NULL;
 
 	return 0;
-}
-
-int matrix_elem_insert(Matrix * restrict matrix, size_t row_idx, size_t col_idx, long double complex value)
-{
-
-}
-
-int matrix_elem_delete(Matrix * restrict matrix, size_t row_idx, size_t col_idx)
-{
-	if (matrix == NULL || value)
-	{
-		return EOF;
-	}
-
-	if (matrix->row && matrix->row != NULL && matrix->col != NULL && matrix->data != NULL)
-	{
-		if (matrix->row_count > row_idx)
-		{	
-			for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1]; ++i)
-			{
-				if (matrix->col[i] == col_idx)
-				{
-					if (matrix->row[matrix->row_count] <= 1u)
-					{
-						matrix->row_count = 0u;
-
-						if (matrix->row != NULL)
-						{
-							free(matrix->row);
-						}
-						matrix->row = NULL;
-
-						if (matrix->col != NULL)
-						{
-							free(matrix->col);
-						}
-						matrix->col = NULL;
-
-						if (matrix->data != NULL)
-						{
-							free(matrix->data);
-						}
-						matrix->data = NULL;
-					}
-					else
-					{/*
-						for (size_t j = i; j < matrix->row[matrix->row_count] - 1u; ++j)
-						{
-							matrix->data[j] = matrix->data[j + 1];
-							matrix->col[j] = matrix->col[j + 1];
-						}
-
-						for (size_t j = i; j < matrix->row[matrix->row_count] - 1u; ++j)
-						{
-							matrix->data[j] = matrix->data[j + 1];
-						}*/
-					}
-				}
-			}
-		}
-	}
-}
-
-int matrix_elem_set(Matrix * restrict matrix, size_t row_idx, size_t col_idx, long double complex value)
-{
-	if (matrix == NULL)
-	{
-		return EOF;
-	}
-
-	else
-	{
-		if (!matrix->row_count || matrix->row == NULL || matrix->col == NULL || matrix->data == NULL)
-		{
-			matrix->row = calloc(row_idx + 2u, sizeof(size_t));
-			if (matrix->row == NULL)
-			{
-				return EOF;
-			}
-
-			matrix->col = calloc(1u, sizeof(size_t));
-			if (matrix->col == NULL)
-			{
-				free(matrix->row);
-				matrix->row = NULL;
-				return EOF;
-			}
-
-			matrix->data = calloc(1u, sizeof(long double complex));
-			if (matrix->data == NULL)
-			{
-				free(matrix->row);
-				matrix->row = NULL;
-
-				free(matrix->col);
-				matrix->col = NULL;
-
-				return EOF;
-			}
-
-			matrix->row_count = row_idx + 1u;
-			matrix->row[row_idx + 1u] = 1;
-			matrix->col[0u] = col_idx;
-			matrix->data[0u] = value;
-		}
-		else
-		{
-			if (matrix->row_count > row_idx)
-			{
-				for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1u]; ++i)
-				{
-					if (matrix->col[i] == col_idx)
-					{
-						matrix->data[i] = value;
-
-						return 0;
-					}
-				}
-			}
-
-			{
-				long double complex *temp = realloc(matrix->data, (matrix->row[matrix->row_count] + 1u) * sizeof(long double complex));
-				if (temp == NULL)
-				{
-					return EOF;
-				}
-				matrix->data = temp;
-			}
-
-			{
-				size_t *temp = realloc(matrix->col, (matrix->row[matrix->row_count] + 1u) * sizeof(size_t));
-				if (temp == NULL)
-				{
-					return EOF;
-				}
-				matrix->col = temp;
-			}
-
-			if (matrix->row_count <= row_idx)
-			{
-				size_t *temp = realloc(matrix->row, (row_idx + 2u) * sizeof(size_t));
-				if (temp == NULL)
-				{
-					return EOF;
-				}
-				matrix->row = temp;
-
-				for (size_t i = matrix->row_count + 1u; i < row_idx + 2u ; ++i)
-				{
-					matrix->row[i] = matrix->row[matrix->row_count];
-				}
-
-				matrix->row_count = row_idx + 1u;
-			}
-
-			for (size_t i = matrix->row[matrix->row_count] - 1u; i >= matrix->row[row_idx]; --i)
-			{
-				matrix->col[i + 1u] = matrix[i];
-			}
-		}
-	}
-
-	return 0;
-}
-
-long double complex matrix_elem_get(const Matrix * restrict matrix, size_t row_idx, size_t col_idx)
-{
-	if ()
-	{
-		/* code */
-	}
-
-	for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1u]; ++i)
-	{
-		if (matrix->col[i] == col_idx)
-		{
-			return matrix->data[i];
-		}
-	}
-
-	return 0.0l;
 }
 
 size_t matrix_get_row_count(const Matrix * restrict matrix)
@@ -246,7 +73,7 @@ size_t matrix_get_col_count(const Matrix * restrict matrix)
 		return (size_t) -1;
 	}
 
-	if (matrix->row == NULL)
+	if (!matrix->row_count || matrix->row == NULL || matrix->data == NULL)
 	{
 		return 0u;
 	}
@@ -256,14 +83,54 @@ size_t matrix_get_col_count(const Matrix * restrict matrix)
 
 		for (size_t i = 0; i < matrix->row[matrix->row_count]; ++i)
 		{
-			if (result < matrix->col[i])
+			if (result < matrix->data[i].column)
 			{
-				result = matrix->col[i];
+				result = matrix->data[i].column;
 			}
 		}
 
 		return ++result;
 	}
+}
+
+int matrix_elem_set(Matrix * restrict matrix, size_t row_idx, size_t col_idx, long double complex value)
+{
+	if (matrix == NULL)
+	{
+		return EOF;
+	}
+
+	if (value)
+	{
+		return matrix_elem_insert(matrix, row_idx, col_idx, value);
+	}
+	else
+	{
+		return matrix_elem_delete(matrix, row_idx, col_idx);
+	}
+}
+
+long double complex matrix_elem_get(const Matrix * restrict matrix, size_t row_idx, size_t col_idx)
+{
+	if (matrix == NULL)
+	{
+		return NAN + NAN * I;
+	}
+
+	if (!matrix->row_count || matrix->data == NULL || matrix->row == NULL)
+	{
+		return 0.0l;
+	}
+
+	for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1u]; ++i)
+	{
+		if (matrix->data[i].column == col_idx)
+		{
+			return matrix->data[i].value;
+		}
+	}
+
+	return 0.0l;
 }
 
 int matrix_fprintf(FILE * restrict stream, const char * restrict format, const Matrix * restrict matrix)
@@ -278,12 +145,256 @@ int matrix_fprintf(FILE * restrict stream, const char * restrict format, const M
 	{
 		for (size_t j = 0u; j < col; ++j)
 		{
-			int local_error = fprintf(stream, format, matrix_elem_get(matrix, i, j));
-
+			int local_error = fprintf(stream, format, creall(matrix_elem_get(matrix, i, j)), cimagl(matrix_elem_get(matrix, i, j)));
 			if (local_error < 0)
 			{
 				return EOF;
 			}
+		}
+		int local_error = fputc('\n', stream);
+		if (local_error < 0)
+		{
+			return EOF;
+		}
+	}
+
+	return 0;
+}
+
+int matrix_sum_max_row_fprint(FILE * restrict stream, const char * restrict format, const Matrix * restrict matrix)
+{
+	if (stream == NULL || format == NULL || matrix == NULL)
+	{
+		return EOF;
+	}
+
+	size_t max_nonzero_count = 0u;
+	for (size_t i = 0u, row = matrix_get_row_count(matrix); i < row; ++i)
+	{
+		size_t nonzero_count = 0u;
+		for (size_t j = 0u, col = matrix_get_col_count(matrix); j < col; ++j)
+		{
+			long double complex temp = matrix_elem_get(matrix, i, j);
+			if (temp != temp)
+			{
+				return EOF;
+			}
+			else if (temp)
+			{
+				++nonzero_count;
+			}
+		}
+		if (nonzero_count > max_nonzero_count)
+		{
+			max_nonzero_count = nonzero_count;
+		}
+	}
+
+	for (size_t i = 0u, row = matrix_get_row_count(matrix); i < row; ++i)
+	{
+		size_t nonzero_count = 0u;
+		for (size_t j = 0u, col = matrix_get_col_count(matrix); j < col; ++j)
+		{
+			long double complex temp = matrix_elem_get(matrix, i, j);
+			if (temp != temp)
+			{
+				return EOF;
+			}
+			else if (temp)
+			{
+				++nonzero_count;
+			}
+		}
+		if (nonzero_count == max_nonzero_count)
+		{
+			long double complex sum = 0.0l;
+			for (size_t j = 0u, col = matrix_get_col_count(matrix); j < col; ++j)
+			{
+				long double complex temp = matrix_elem_get(matrix, i, j);
+				if (temp != temp)
+				{
+					return EOF;
+				}
+				sum += temp;
+			}
+
+			int local_error = fprintf(stdout, format, i, sum);
+			if (local_error < 0)
+			{
+				return EOF;
+			}
+		}
+	}
+	return 0;
+}
+
+int matrix_elem_insert(Matrix * restrict matrix, size_t row_idx, size_t col_idx, long double complex value)
+{
+	if (matrix == NULL)
+	{
+		return EOF;
+	}
+
+	if (!matrix->row_count || matrix->row == NULL || matrix->data == NULL)
+	{
+		matrix->row = calloc(row_idx + 2u, sizeof(size_t));
+		if (matrix->row == NULL)
+		{
+			return EOF;
+		}
+
+		matrix->data = calloc(1u, sizeof(Pair));
+		if (matrix->data == NULL)
+		{
+			free(matrix->row);
+			matrix->row = NULL;
+
+			return EOF;
+		}
+
+		matrix->row_count = row_idx + 1u;
+		matrix->row[row_idx + 1u] = 1;
+		matrix->data[0u] = (Pair) { col_idx, value };
+
+		return 0;
+	}
+
+	if (matrix->row_count > row_idx)
+	{
+		for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1u]; ++i)
+		{
+			if (matrix->data[i].column == col_idx)
+			{
+				matrix->data[i] = (Pair) { col_idx, value };
+				return 0;
+			}
+		}
+	}
+
+	Pair *temp = realloc(matrix->data, (matrix->row[matrix->row_count] + 1u) * sizeof(Pair));
+	if (temp == NULL)
+	{
+		return EOF;
+	}
+	matrix->data = temp;
+
+	if (matrix->row_count <= row_idx)
+	{
+		size_t *temp = realloc(matrix->row, (row_idx + 2u) * sizeof(size_t));
+		if (temp == NULL)
+		{
+			return EOF;
+		}
+		matrix->row = temp;
+
+		for (size_t i = matrix->row_count + 1u; i < row_idx + 2u ; ++i)
+		{
+			matrix->row[i] = matrix->row[matrix->row_count];
+		}
+
+		matrix->row_count = row_idx + 1u;
+		++matrix->row[matrix->row_count];
+		matrix->data[matrix->row[row_idx]] = (Pair) { col_idx, value };
+
+		return 0;
+	}
+
+	for (size_t i = row_idx + 1u; i <= matrix->row_count; ++i)
+	{
+		++matrix->row[i];
+	}
+
+	for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1u]; ++i)
+	{
+		if (col_idx < matrix->data[i].column || i == matrix->row[row_idx + 1u] - 1u)
+		{
+			for (size_t j = matrix->row[matrix->row_count] - 2u; j >= i && j < (size_t) -1; --j)
+			{
+				matrix->data[j + 1] = matrix->data[j];
+			}
+			matrix->data[i] = (Pair) { col_idx, value };
+			break;
+		}
+	}
+
+	return 0;
+}
+
+int matrix_elem_delete(Matrix * restrict matrix, size_t row_idx, size_t col_idx)
+{
+	if (matrix == NULL)
+	{
+		return EOF;
+	}
+
+	if (!matrix->row || matrix->row == NULL || matrix->data == NULL)
+	{
+		return 0;
+	}
+
+	if (matrix->row_count <= row_idx)
+	{
+		return 0;
+	}
+
+	for (size_t i = matrix->row[row_idx]; i < matrix->row[row_idx + 1]; ++i)
+	{
+		if (matrix->data[i].column == col_idx)
+		{
+			if (matrix->row[matrix->row_count] <= 1u)
+			{
+				matrix->row_count = 0u;
+
+				if (matrix->row != NULL)
+				{
+					free(matrix->row);
+				}
+				matrix->row = NULL;
+
+				if (matrix->data != NULL)
+				{
+					free(matrix->data);
+				}
+				matrix->data = NULL;
+
+				return 0;
+			}
+
+			for (size_t j = i; j < matrix->row[matrix->row_count] - 1u; ++j)
+			{
+				matrix->data[j] = matrix->data[j + 1u];
+			}
+
+			for (size_t j = row_idx + 1u; j <= matrix->row_count; ++j)
+			{
+				--matrix->row[j];
+			}
+
+			Pair *temp = realloc(matrix->data, matrix->row[matrix->row_count] * sizeof(Pair));
+			if (temp == NULL)
+			{
+				return EOF;
+			}
+			matrix->data = temp;
+
+			if (matrix->row_count == row_idx + 1u)
+			{
+				size_t j = matrix->row_count;
+				while (matrix->row[j] >= matrix->row[matrix->row_count])
+				{
+					--j;
+				}
+
+				matrix->row_count = j + 1u;
+
+				size_t *temp = realloc(matrix->row, (j + 2u) * sizeof(Pair));
+				if (temp == NULL)
+				{
+					return EOF;
+				}
+				matrix->row = temp;
+			}
+			break;
 		}
 	}
 
@@ -295,14 +406,16 @@ void debug_fprintf(const Matrix * restrict matrix)
 	printf("matrix = %p\n", (void *) matrix);
 	if (matrix == NULL)
 	{
+		putchar('\n');
 		return;
 	}
 
 	printf("matrix->row_count = %zu\n", matrix->row_count);
-	printf("matrix->row = %p, matrix->col = %p, matrix->data = %p\n", (void *) matrix->row, (void *) matrix->col, (void *) matrix->data);
+	printf("matrix->row = %p, matrix->data = %p\n", (void *) matrix->row, (void *) matrix->data);
 
-	if (matrix->row == NULL || matrix->col == NULL || matrix->data == NULL)
+	if (!matrix->row_count || matrix->row == NULL || matrix->data == NULL)
 	{
+		putchar('\n');
 		return;
 	}
 
@@ -313,17 +426,18 @@ void debug_fprintf(const Matrix * restrict matrix)
 	}
 	printf(" }\n");
 
-	printf("matrix->col  = {");
+	printf("matrix->col = {");
 	for (size_t i = 0u; i < matrix->row[matrix->row_count]; ++i)
 	{
-		printf(" %010zu,", matrix->col[i]);
+		printf(" %zu,", matrix->data[i].column);
 	}
 	printf(" }\n");
 
 	printf("matrix->data = {");
 	for (size_t i = 0u; i < matrix->row[matrix->row_count]; ++i)
 	{
-		printf("%.2Lf + %.2Lfi,", creall(matrix->data[i]), cimagl(matrix->data[i]));
+		printf(" %.2Lf + %.2Lfi,", creall(matrix->data[i].value), cimagl(matrix->data[i].value));
 	}
 	printf(" }\n");
+	putchar('\n');
 }
